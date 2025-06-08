ceebe124885eff5de4974daf4158919e166c8fc3
as <a.suzuki.p.8888@gmail.com>
2025-06-08 22:15:29 +0900

    tailwind cssモジュールの設定変更

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
 // tailwind.config.js
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -7,5 +7,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
